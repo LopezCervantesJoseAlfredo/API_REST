233d949eb5ca96cb2e31dee6cb9ae54d513f6bea
LopezCervantesJoseAlfredo <Lopezcervantesjosealfredo@gmail.com>
2024-04-27 23:11:57 -0700

    commit

--- API_REST/Unidad 3/Test/src/index.js
+++
@@ -1,4 +0,0 @@
-import * as ternurines from './modulo.js';
-
-let resultado = ternurines.randomTernurin();
-console.log(resultado);
--- API_REST/Unidad 3/Test/test/test.js
+++
@@ -1,8 +0,0 @@
-import assert from 'node:assert';
-import test from 'node:test';
-import * as ternurin from '../src/modulo.js';
-
-test('el ternurin está dentro de la lista', () => {
-    let resultado = 'Avril Husky';
-    assert.ok(ternurin.ternurines.includes(resultado));
-});
